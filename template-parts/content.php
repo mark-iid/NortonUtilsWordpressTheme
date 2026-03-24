696d131ec3eb992b007b4d420e21e97b5e2acaca
Mark Earnest <mark@mystikos.org>
2026-03-24 10:41:35 -0400

    don't assume summary on homepage

--- template-parts/content.php
+++ template-parts/content.php
@@ -7,27 +7,21 @@
         <?php endif; ?>
     </h2>
 
-    <?php if ( ! is_singular() ) : ?>
-        <div class="entry-summary">
-            <?php the_excerpt(); ?>
-        </div>
-    <?php else : ?>
-        <div class="entry-content">
-            <?php
-            the_content(
-                sprintf(
-                    wp_kses(
-                        __( '[READ MORE <span class="screen-reader-text">"%1$s"</span>]', 'norton-simple' ),
-                        array( 'span' => array( 'class' => array() ) )
-                    ),
-                    get_the_title()
-                )
-            );
-            wp_link_pages( array(
-                'before' => '<div class="page-links">' . esc_html__( '[PAGES]', 'norton-simple' ),
-                'after'  => '</div>',
-            ) );
-            ?>
-        </div>
-    <?php endif; ?>
+    <div class="entry-content">
+        <?php
+        the_content(
+            sprintf(
+                wp_kses(
+                    __( '[READ MORE <span class="screen-reader-text">"%1$s"</span>]', 'norton-simple' ),
+                    array( 'span' => array( 'class' => array() ) )
+                ),
+                get_the_title()
+            )
+        );
+        wp_link_pages( array(
+            'before' => '<div class="page-links">' . esc_html__( '[PAGES]', 'norton-simple' ),
+            'after'  => '</div>',
+        ) );
+        ?>
+    </div>
 </article>
